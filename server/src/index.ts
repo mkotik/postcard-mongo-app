93fe914c7c4521c4706bfcb4e4c51c94a9f723ea
Marat Kotik <mkotik97@gmail.com>
2022-11-15 18:39:13 -0500

    added .env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Express } from "express";
+require("dotenv").config();
 const app: Express = express();
 
 app.use(express.json());
+
+const PORT = process.env.PORT ?? 8000;
 
 app.get("/test", (req, res) => {
   const name: string = "Marat";
   res.send(`Hi ${name}`);
 });
 
-app.listen(8000, () => {
-  console.log("listening on port 8000");
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
